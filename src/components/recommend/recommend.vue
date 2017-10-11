ccfe43c285529f5bd9d4fa81762425a99a7db334
wq93 <214180117@qq.com>
2017-10-11 10:05:59 +0800

    利用better-scroll创建初步完成可滑动轮播图

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper"></div>
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul>
@@ -13,10 +21,17 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from '../../base/slider/slider.vue'
+
   import {getRecommend} from '../../api/recommend'
   import {ERR_OK} from '../../api/config'
 
   export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
     created() { // 生命周期钩子
       this._getRecommend()
     },
@@ -24,10 +39,13 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 </script>
