dee62ebbe037b229ad2b1ff9fb850f962889c0c6
wq93 <214180117@qq.com>
2017-10-16 16:21:06 +0800

    注册VueLazyLoad插件

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick' // 解决移动端300毫秒延迟
+import VueLazyLoad from 'vue-lazyload'
 Vue.config.productionTip = false
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
