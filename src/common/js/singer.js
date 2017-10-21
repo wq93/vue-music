f422722ad5233bc2ec3d0dc721ecc11758d2e3ee
wq93 <214180117@qq.com>
2017-10-21 22:49:04 +0800

    *********歌手模块-start***********           按歌手拼音首字符过滤数据

---
+++ src/common/js/singer.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wangqi on 2017/10/21.
+ */
+export default class Singer {
+  constructor({id, name}) {
+    this.id = id
+    this.name = name
+    this.avatar = `https://y.gtimg.cn/music/photo_new/T001R300x300M000${id}.jpg?max_age=2592000`
+  }
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,7 +7,9 @@
 <script type="text/ecmascript-6">
   import {getSingerList} from '../../api/singer'
   import {ERR_OK} from '../../api/config'
-
+  import Singer from '../../common/js/singer' // 封装的热门歌手信息的class
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
   export default {
     data() {
       return {
@@ -22,8 +24,38 @@
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
             this.singers = res.data.list
-            console.log(this.singers)
+            console.log(this._normalizeSinger(this.singers))
           }
+        })
+      },
+      _normalizeSinger(list) { // 过滤热门歌手的数据
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          // 按歌手拼音首字符分类
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          // 把开头字母一类的歌手归类
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+          console.log(map)
         })
       }
     }
