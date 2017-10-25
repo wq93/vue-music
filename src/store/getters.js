3f687bf794abbe7025f6021e98b4cde4265859fb
wq93 <214180117@qq.com>
2017-10-25 18:14:03 +0800

    播放器模块-配置播放器需要的store数据(vuex)

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,3 +2,20 @@
  * 获取mutations
  * */
 export const singer = state => state.singer
+
+export const palying = state => state.palying
+
+export const fullScreen = state => state.fullScreen
+
+export const playlist = state => state.playlist
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+export const currentSong = (state) => {
+  return state.playlist[state.currentIndex] // 根据索引获取歌曲
+}
+
--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,15 @@
 /**
  * 数据状态
  */
+import {playMode} from '../common/js/config'
+
 const state = {
-  singer: {}
+  singer: {}, // 歌手详情信息
+  playing: false, // 播放状态
+  fullScreen: false, // 播放器是否全屏
+  playlist: [], // 歌曲列表
+  sequenceList: [], // 排序的歌曲列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1
 }
 export default state
