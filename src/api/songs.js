63119119b2d1800e9bc656d3eff5ceb09b1f5fb2
wq93 <214180117@qq.com>
2017-10-29 07:49:16 +0800

    播放器模块-抓取歌词数据

---
+++ src/api/songs.js
@@ -0,0 +1,21 @@
+// 抓取歌词数据
+import {commonParams} from './config'
+import axios from 'axios'
+
+export function getLyric() {
+  const url = '/api/lyric'
+  const data = Object.assign({}, commonParams, {
+    songmid: mid,
+    platform: 'yqq',
+    hostUin: 0,
+    needNewCode: 0,
+    categoryId: 10000000,
+    pcachetime: +new Date(),
+    format: 'json'
+  })
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
